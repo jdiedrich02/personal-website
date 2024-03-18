ce449e4f57ba02c4ad4b69e8229c62bf660d6455
Darkkz <jake.diedrich@gmail.com>
2024-03-17 20:18:21 -0400

    Fix Astro Links

--- scripts/astro.js
+++ scripts/astro.js
@@ -1,11 +1,11 @@
 const imageData = [
     {
-        imageUrl: '../images/Betel.png',
+        imageUrl: './images/Betel.png',
         title: 'Betelgeuse Star',
         description: 'This is a star located in the constellation Orion. This star is located around 640 light years from Earth and its radius is about 617 million km! Fun fact: This star is predicted to go supernova soon. This will be one of the brightest objects in the sky when it does.'
     },
     {
-        imageUrl: '../images/CrabNebula.jpg',
+        imageUrl: './images/CrabNebula.jpg',
         title: 'M1 - Crab Nebula',
         description: 'Located in the constellation Taurus, we have the Crab Nebula! This nebula is located around 6500 light years from Earth. This is a supernova remnant that was actually seen by astronomers around 1000 years ago!'
     },
@@ -15,27 +15,27 @@ const imageData = [
         description: 'My first galaxy photo! This is a spiral galaxy located around 2.73 million light years from Earth. This galaxy houses around 40 billion stars and is about 60,000 light years in diameter!'
     },
     {
-        imageUrl: '../images/M36.jpg',
+        imageUrl: './images/M36.jpg',
         title: 'M36 - Star Cluster',
         description: 'Next we have the M36 star cluster! This is a open cluster of many stars. It is also known as the pinwheel cluster. It has a diameter of about 14 light years!'
     },
     {
-        imageUrl: '../images/M37_Edit.jpg',
+        imageUrl: './images/M37_Edit.jpg',
         title: 'M37 - Star Cluster',
         description: 'One of the brighter star clusters in the sky. This cluster is located about 4500 light years from Earth! Its diameter is around 20-26 light years and in the middle it houses around 500 stars!'
     },
     {
-        imageUrl: '../images/M45 Edit.jpg',
+        imageUrl: './images/M45 Edit.jpg',
         title: 'M45 Star Cluster - Pleiades',
         description: 'One of the most popular deep sky objects to image! It is commonly refered to as the seven sisters. Unfortunately for my photo, I was only able to capture 4 of the main stars. Located around 444 light years from Earth!'
     },
     {
-        imageUrl: '../images/OrionEdited.jpg',
+        imageUrl: './images/OrionEdited.jpg',
         title: 'Orion Nebula',
         description: 'One of the most popular Nebulas in the sky! This is located in the Orion constellation and is located around 1350 light years from Earth! This objects is actually visible with the naked eye! It has a diameter of around 24 light years!'
     },
     {
-        imageUrl: '../images/M51.jpg',
+        imageUrl: './images/M51.jpg',
         title: 'M51 - Whirlpool Galaxy',
         description: 'Another one of my galaxy shots! Here we have M51! This was acctually the first galaxy to be classfied as spiral! This galaxy is located about 31 million light years from Earth! This means we are seeing it as it was 31 million years ago! It contains around 100 billion stars and its diameter is around 60,000 light years!'
     },
